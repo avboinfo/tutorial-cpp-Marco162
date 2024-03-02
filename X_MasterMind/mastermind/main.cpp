215c0aa654a70a1280b3db71b225340557d2aa79
Marco162 <153986195+Marco162@users.noreply.github.com>
2024-03-02 11:27:28 +0000

    inizio gioco mastermind

---
+++ X_MasterMind/mastermind/main.cpp
@@ -0,0 +1,10 @@
+#include "mastermind.cpp"
+
+int main(int argc, char const *argv[])
+{
+    mastermind prova = mastermind();
+
+    prova.nuova_giocata();
+
+    return 0;
+}
---
+++ X_MasterMind/mastermind/mastermind.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+class mastermind
+{
+private:
+    int numero_mosse;
+    std::string ultima_mossa;
+    int codice_segreto[DIM_GIOCATA_VALIDA];
+
+    static const int DIM_GIOCATA_VALIDA = 4;
+    int mossa_valida[DIM_GIOCATA_VALIDA];
+
+    bool sanifica_input(){
+        if(DIM_GIOCATA_VALIDA != ultima_mossa.size())
+        return false;
+
+        for(int i = 0; i < ultima_mossa.size(); i++)
+        {
+            char c = ultima_mossa[i];
+
+            if(c >= '0' && c <= '9'){
+                mossa_valida[i] = c - '0';
+            } else if(c == '-'){ // il trattino verrà convertito in -1
+                mossa_valida[i] = -1;
+            } else{
+                return false;
+            }
+        }
+        return true;
+ 
+    }
+    
+    void genera_codice_segreto(){
+        srand(time(NULL));
+        for(int i = 0; i < DIM_GIOCATA_VALIDA; i++)
+        {
+            codice_segreto[i] = rand()%10;
+        }
+    }
+
+public:
+
+    mastermind(){
+        numero_mosse = 0;
+        ultima_mossa = "";
+    };
+
+    void nuova_giocata(){
+        
+        do{
+            std::cout<< numero_mosse + 1 << ": ";
+            std::getline(std::cin, ultima_mossa); // std::cin << ultima_mossa << std::endl; non è in grado di leggere gli  
+        } while (!sanifica_input());
+
+        numero_mosse++;    
+    }
+
+    void risultato_mossa(){
+        //... code ...
+    }
+
+};
+
